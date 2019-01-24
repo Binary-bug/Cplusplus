928844ef6e0bcd1094c0d056e701e04e3a535211
BinaryBug <BinaryBug@lubuntu.com>
2019-01-24 19:41:03 +0530

    end of chapter 7

--- acpp/chp7/sentencegen.cpp
+++ acpp/chp7/sentencegen.cpp
@@ -1,8 +1,28 @@
+#include<string>
+#include<vector>
+#include "split.h"
+#include<map>
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
 typedef vector<string> Rule;
 typedef vector<Rule> Rule_collection;
 typedef map<string,Rule_collection> Grammar;
 
-
+int nrand(int n){
+	if (n <=0 || n > RAND_MAX){
+		throw domain_error("Argument to nrand is out of range");
+	}
+	const int bucket_size = RAND_MAX / n;
+	int r;
+	
+	do r = rand() / bucket_size;
+	while(r >= n);
+	
+	return r;
+}
 
 
 bool bracketed(const string& s){
@@ -11,7 +31,7 @@ bool bracketed(const string& s){
 
 
 
-void gen_aux(const Grammar& g; const string& word, vector<string>& ret){
+void gen_aux(const Grammar& g, const string& word, vector<string>& ret){
 	if(!bracketed(word))
 		ret.push_back(word);
 	else{
@@ -54,4 +74,24 @@ vector<string> gen_sen(const Grammar& g){
 	vector<string> ret;
 	gen_aux(g,"<sentence>",ret);
 	return ret;
+}
+
+int main(){
+	//generate the sentence
+	vector<string> sentence = gen_sen(read_grammar(cin));
+	
+	// write the first word, if any
+	vector<string>::const_iterator it =sentence.begin();
+	if(!sentence.empty()){
+		cout << *it;
+		++it;
+	}
+	
+	// write the rest of the words, each preceded by a space
+	while(it != sentence.end()){
+	cout << " " << *it;
+	++it;
+	}
+	cout << endl;
+	return 0;
 }
